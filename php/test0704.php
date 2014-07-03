eef370b885c20565048013536371ed7d2b75ff59
TzuPingWu <bottle7839@hotmail.com>
2014-07-03 22:24:52 +0800

    The php file with count.txt

--- php/test0704.php
+++ php/test0704.php
@@ -1,14 +1,28 @@
-<?php
-
-?>
 <html>
 <head>
 <title>Test File</title>
 </head>
 <body>
-<p> This is the test file	</p>
-
+<p> This is the test file.</p>
+<p> The count in the file is....
+<?php
+$temp = 0;
+$file = fopen("count.txt","r+") or die("Unable to read the file.");
+$temp = fgets($file);
+echo $temp;
+?></p>
+<p> The count after +1 = 
+<?php
+$wfile = fopen("count.txt","w+") or die("Unable to write the file.");
+if (flock($wfile,LOCK_EX)){
+	$num = $temp+1;	
+	echo $num; 
+	fwrite($wfile,$num);
+	flock($wfile,LOCK_UN);//critical section unlock
+	fclose($wfile);//close the file
+	}
+else{
+	echo "Error locking file!";
+	}?></p>
 </body>
 </html>
-<?php
-?>
